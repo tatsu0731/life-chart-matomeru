581275f8d5cb6baa1548f4f0c65683e0487ec6ca
Tatsuhiro Motomatsu <tatsuhiro.motomatsu@posse-ap.com>
2025-02-02 14:06:00 +0900

    add: headerとfooterを作成

---
+++ src/components/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer className="flex justify-center">
+            <p className="text-sm ">&copy; life-chart matomeru 2025</p>
+        </footer>
+    );
+}
