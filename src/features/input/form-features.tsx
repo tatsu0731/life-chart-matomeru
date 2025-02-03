dcc9c9f837d44704b31c945c24ff45db5ae55bee
Tatsuhiro Motomatsu <tatsuhiro.motomatsu@posse-ap.com>
2025-02-03 14:18:49 +0900

    fix: フォームのサイズを調節

--- src/features/input/form-features.tsx
+++ src/features/input/form-features.tsx
@@ -42,17 +42,17 @@ export default function FormFeature() {
     return (
         <form
             onSubmit={handleSubmit(onSubmit)}
-            className="flex flex-col justify-center gap-8 py-8 w-96">
+            className="flex flex-col justify-center gap-8 py-8 w-2/3">
             <div className="flex flex-col gap-4">
             {fields.map((field, index) => {
                 return (
                     <div key={field.id}>
-                        <div className="border-2 border-yellow-300 py-4 px-4 rounded-lg">
+                        <div className="border-2 border-yellow-300 py-4 px-4 rounded-lg flex flex-col gap-4">
                             <div className="flex justify-between">
                                 <div className="text-xs text-yellow-500">■ {index + 1}歳</div>
                                 {fields.length - 1 === index && <p onClick={() => remove(index)} className="text-red-400 cursor-pointer">削除</p>}
                             </div>
-                            <label htmlFor={`items.${index}.value`} className="text-sm text-gray-600 font-bold">
+                            <label htmlFor={`items.${index}.value`} className="text-sm text-gray-600 font-bold flex flex-col">
                                 評価
                                 <select id={`items.${index}.value`}
                                 required
@@ -60,14 +60,14 @@ export default function FormFeature() {
                                     {...register(
                                         `items.${index}.value`
                                     )}
-                                className="border-2 py-1 px-2 rounded-md"
+                                className="border-2 py-1 px-2 rounded-md w-14"
                                 >
                                     {[...Array(10)].map((_, i) => (
                                         <option key={i} value={i + 1}>{i + 1}</option>
                                     ))}
                                 </select>
                             </label>
-                            <label htmlFor={`items.${index}.description`} className="text-sm text-gray-600 font-bold">
+                            <label htmlFor={`items.${index}.description`} className="text-sm text-gray-600 font-bold flex flex-col">
                                 出来事
                                 <input
                                     required
